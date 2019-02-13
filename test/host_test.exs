e32e8798784f7ab50cf2e67813e8c3c34fa567ac
Robb Shecter <robb@public.law>
2019-02-13 00:06:38 -0800

    refactor: describe blocks

--- test/host_test.exs
+++ test/host_test.exs
@@ -1,32 +1,34 @@
 defmodule HostTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Host
 
-  test "can identify localhost" do
-    assert Host.reverse_lookup(ip: "127.0.0.1") == {:ok, "localhost"}
+  describe "reverse_lookup/1" do
+    test "can identify localhost" do
+      assert Host.reverse_lookup(ip: "127.0.0.1") == {:ok, "localhost"}
+    end
+
+    test "malformed ip produces an error" do
+      assert {:error, _message} = Host.reverse_lookup(ip: "1.2.3.4.5")
+    end
+
+    test "works with standard ip structure" do
+      assert Host.reverse_lookup(ip: {127, 0, 0, 1}) == {:ok, "localhost"}
+    end
   end
 
-  test "malformed ip produces an error" do
-    assert {:error, _message} = Host.reverse_lookup(ip: "1.2.3.4.5")
+  describe "ext_reverse_lookup/1" do
+    test "functions like reverse_lookup() when host is found" do
+      assert Host.ext_reverse_lookup(ip: "127.0.0.1") == {:ok, "localhost"}
+    end
+
+    test "malformed ip produces an error here too" do
+      assert {:error, _message} = Host.ext_reverse_lookup(ip: "1.2.3.4.5")
+    end
   end
 
-  test "works with standard ip structure" do
-    assert Host.reverse_lookup(ip: {127, 0, 0, 1}) == {:ok, "localhost"}
-  end
-
-  #
-  # ext_reverse_lookup()
-  #
-
-  test "functions like reverse_lookup() when host is found" do
-    assert Host.ext_reverse_lookup(ip: "127.0.0.1") == {:ok, "localhost"}
-  end
-
-  test "malformed ip produces an error here too" do
-    assert {:error, _message} = Host.ext_reverse_lookup(ip: "1.2.3.4.5")
-  end
-
-  test "it creates a ptr domain" do
-    assert Host.ptr_domain("1.2.3.4") == "4.3.2.1.in-addr.arpa"
+  describe "ptr_domain/1" do
+    test "it creates a ptr domain" do
+      assert Host.ptr_domain("1.2.3.4") == "4.3.2.1.in-addr.arpa"
+    end
   end
 end
