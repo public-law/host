7d5bea4faa0720f8649da7dec4e06d8ae55c3fcb
Robb Shecter <robb@public.law>
2019-02-13 14:51:59 -0800

    better guards

--- lib/ip_v4.ex
+++ lib/ip_v4.ex
@@ -2,7 +2,14 @@ defmodule IPv4 do
   @enforce_keys [:octets]
   defstruct [:octets]
 
-  def new(a, b, c, d) when is_integer(a) and is_integer(b) and is_integer(c) and is_integer(d) do
+  # A complex guard
+  defmacro is_octet(number) do
+    quote do
+      is_integer(unquote(number)) and unquote(number) >= 0 and unquote(number) <= 255
+    end
+  end
+
+  def new(a, b, c, d) when is_octet(a) and is_octet(b) and is_octet(c) and is_octet(d) do
     %IPv4{octets: {a, b, c, d}}
   end
 
