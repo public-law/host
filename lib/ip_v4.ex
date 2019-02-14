6ab4938321dcf31be46525711153ad943eec04c2
Robb Shecter <robb@public.law>
2019-02-13 17:28:55 -0800

    refactor: another defguard

--- lib/ip_v4.ex
+++ lib/ip_v4.ex
@@ -3,13 +3,14 @@ defmodule IPv4 do
   defstruct [:octets]
 
   defguard is_octet(number) when is_integer(number) and number >= 0 and number <= 255
+  defguard are_octets(a, b, c, d) when is_octet(a) and is_octet(b) and is_octet(c) and is_octet(d)
 
   def new_from_string(ip) when is_bitstring(ip) do
     [a, b, c, d] = String.split(ip, ".") |> Enum.map(&String.to_integer/1)
     new(a, b, c, d)
   end
 
-  def new(a, b, c, d) when is_octet(a) and is_octet(b) and is_octet(c) and is_octet(d) do
+  def new(a, b, c, d) when are_octets(a, b, c, d) do
     %IPv4{octets: {a, b, c, d}}
   end
 
